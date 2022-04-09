90231c71ef400d2115819e02283cbc130f5fd946
AbelCodeUp <bxk0129@163.com>
2022-04-09 21:38:10 +0800

    update:checklist

--- src/views/Mine/Mine.tsx
+++ src/views/Mine/Mine.tsx
@@ -1,22 +1,46 @@
 import "./Mine.scss";
 import MineBanner from "../../assets/images/mine/mine-banner.png";
-import { memo, SetStateAction, useEffect, useState } from "react";
+import { memo, SetStateAction, useEffect, useState, MouseEvent } from "react";
 import { useHistory } from "react-router-dom";
 // import { Paper } from "@olympusdao/component-library";
-import { Container, useMediaQuery, Box, Grid, makeStyles, Button, Tabs, Tab } from "@material-ui/core";
+import {
+  Container,
+  useMediaQuery,
+  Box,
+  Grid,
+  Button,
+  Tabs,
+  Tab,
+  Popover,
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
+  ListItemAvatar,
+  Checkbox,
+  Avatar,
+} from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { ethers } from "ethers";
 import { NFTMiner_ABI, NFTMiner_ADDRESS, MBTCStaking_ADDRESS, MBTCStaking_ABI, POOL_ID } from "src/contract";
 import { TabPanel, TabContext } from "@material-ui/lab";
 
-const useStyles = makeStyles({
-  // 卡片按钮 black white
-  blackCardBtnOrange: {
-    background: "#d6a241",
-  },
-  blackCardBtnBlue: {
-    background: "#6a40bb",
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+    blackCardBtnOrange: {
+      background: "#d6a241",
+    },
+    blackCardBtnBlue: {
+      background: "#6a40bb",
+    },
+  }),
+);
+
 interface NftType {
   name: string;
   image: string;
@@ -30,16 +54,59 @@ const Mine: React.FC = () => {
   const history = useHistory();
   // const { address, connect, provider, connected, networkId, providerInitialized } = useWeb3Context();
   // usePathForNetwork({ pathName: "mine", networkID: networkId, history });
-
+  const classes = useStyles();
   const isSmallScreen = useMediaQuery("(max-width: 650px)");
   const isVerySmallScreen = useMediaQuery("(max-width: 379px)");
   const [unStakedList, setUnStakedList] = useState<NftType[]>();
   const [stakedList, setStakedList] = useState<NftType[]>();
+  const [checkList, setCheckList] = useState<string[]>([]); //选项checklist
   const [value, setValue] = useState("1");
-
   const handleChange = (event: any, newValue: string) => {
     setValue(newValue);
   };
+
+  // 下拉弹框 start
+  const [minerItem, setMinerItem] = useState<NftType[]>();
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const clearCheckList = () => {
+    setCheckList([]);
+  };
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>, value: string) => {
+    setAnchorEl(event.currentTarget);
+    clearCheckList();
+    if (value === "1") {
+      // console.log("stakedList", stakedList);
+      setMinerItem(stakedList);
+    } else {
+      setMinerItem(unStakedList);
+    }
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    // 选中的stakedID
+    console.log("checkList", checkList);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+  // 下拉弹框 end
+
+  // 多选 start
+
+  const handleToggle = (value: string) => () => {
+    const currentIndex = checkList.indexOf(value);
+    const newChecked = [...checkList];
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+    setCheckList(newChecked);
+  };
+  // 多选 end
 
   // 获取合约签名
   const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -237,15 +304,26 @@ const Mine: React.FC = () => {
               }}
               aria-label="basic tabs example"
             >
-              <Tab label="Staked Miners" value="1" />
-              <Tab label="UnStaked Miners" value="2" />
+              <Tab
+                className={`${isSmallScreen || isVerySmallScreen ? "tab-mobile" : ""}`}
+                onClick={e => handleClick(e, "1")}
+                aria-describedby={id}
+                label="Staked Miners"
+                value="1"
+              />
+              <Tab
+                className={`${isSmallScreen || isVerySmallScreen ? "tab-mobile" : ""}`}
+                onClick={e => handleClick(e, "2")}
+                label="UnStaked Miners"
+                value="2"
+              />
             </Tabs>
           </Box>
           <TabPanel value="1">
             <Grid container spacing={3}>
               <Grid item xs={12}>
-                <Box className="btc-item-title-container">
-                  <Box className="btc-item-right-container">
+                <Box className={`btc-item-title-container ${isSmallScreen || isVerySmallScreen ? "" : "pc"}`}>
+                  <Box className={`btc-item-right-container ${isSmallScreen || isVerySmallScreen ? "" : "pc"}`}>
                     <Box className="btc-item-right-btn">Untake Miners</Box>
                     <Box className="btc-item-right-btn">Unstake All</Box>
                   </Box>
@@ -256,61 +334,67 @@ const Mine: React.FC = () => {
                 xs={12}
                 style={{
                   paddingLeft: isSmallScreen || isVerySmallScreen ? ".6rem" : "1.4rem",
-                  paddingRight: isSmallScreen || isVerySmallScreen ? ".3rem" : "1.4rem",
+                  paddingRight: isSmallScreen || isVerySmallScreen ? ".6rem" : "1.4rem",
                   paddingTop: "1.6rem",
                   paddingBottom: "1.6rem",
                 }}
+                spacing={5}
               >
-                {stakedList?.map(item => (
-                  <Box
-                    className="btc-card-item"
-                    style={{
-                      marginRight: isSmallScreen || isVerySmallScreen ? ".5rem" : "1.6rem",
-                      marginBottom: isSmallScreen || isVerySmallScreen ? ".5rem" : "1.6rem",
-                    }}
-                  >
-                    <Box className="btc-card-item-img">
-                      <img src={item.image} alt="" />
-                    </Box>
-                    <Box className="btc-card-item-desc">
-                      <Box className="btc-card-item-desc-title">{item.name}</Box>
-                      <Box className="btc-card-item-desc-price-box">
-                        <Box
-                          className="btc-card-item-desc-price-item"
-                          display="flex"
-                          alignItems="center"
-                          justifyContent="space-between"
-                        >
-                          <Box display="flex" alignItems="center">
-                            <Box className="item-title">MBTC Mined</Box>
-                            <Box className="item-price">{item.mined}</Box>
-                          </Box>
-                          <div className={`btn ${true ? "orange" : "blue"}`}>Harvest</div>
+                <Grid className="stake-container" container spacing={1} justifyContent="space-between">
+                  {stakedList?.map(item => (
+                    <Grid item>
+                      <Box
+                        className="btc-card-item"
+                        style={{
+                          // marginRight: isSmallScreen || isVerySmallScreen ? ".5rem" : "1.6rem",
+                          marginBottom: isSmallScreen || isVerySmallScreen ? "1rem" : "1.6rem",
+                        }}
+                      >
+                        <Box className="btc-card-item-img">
+                          <img src={item.image} alt="" />
                         </Box>
-                        <Box
-                          className="btc-card-item-desc-price-item"
-                          display="flex"
-                          alignItems="center"
-                          justifyContent="space-between"
-                        >
-                          <Box display="flex" alignItems="center">
-                            <Box className="item-title">MFuel Cost</Box>
-                            <Box className="item-price">{item.cost}</Box>
+                        <Box className="btc-card-item-desc">
+                          <Box className="btc-card-item-desc-title">{item.name}</Box>
+                          <Box className="btc-card-item-desc-price-box">
+                            <Box
+                              className="btc-card-item-desc-price-item"
+                              display="flex"
+                              alignItems="center"
+                              justifyContent="space-between"
+                            >
+                              <Box display="flex" flexDirection="column">
+                                <Box className="item-title">MBTC Mined</Box>
+                                <Box className="item-price">{item.mined}</Box>
+                              </Box>
+                              <div className={`btn ${true ? "orange" : "blue"}`}>Harvest</div>
+                            </Box>
+                            <Box
+                              className="btc-card-item-desc-price-item"
+                              display="flex"
+                              alignItems="center"
+                              justifyContent="space-between"
+                            >
+                              <Box display="flex" flexDirection="column">
+                                <Box className="item-title">MFuel Cost</Box>
+                                <Box className="item-price">{item.cost}</Box>
+                              </Box>
+                              <div className={`btn ${false ? "orange" : "blue"}`}>Unstake</div>
+                            </Box>
                           </Box>
-                          <div className={`btn ${false ? "orange" : "blue"}`}>Unstake</div>
                         </Box>
                       </Box>
-                    </Box>
-                  </Box>
-                ))}
+                    </Grid>
+                  ))}
+                  {!stakedList || stakedList.length == 0 ? <Box>stakedList</Box> : undefined}
+                </Grid>
               </Grid>
             </Grid>
           </TabPanel>
           <TabPanel value="2">
             <Grid container spacing={3}>
               <Grid item xs={12}>
-                <Box className="btc-item-title-container">
-                  <Box className="btc-item-right-container">
+                <Box className={`btc-item-title-container ${isSmallScreen || isVerySmallScreen ? "" : "pc"}`}>
+                  <Box className={`btc-item-right-container ${isSmallScreen || isVerySmallScreen ? "" : "pc"}`}>
                     <Box className="btc-item-right-btn">Stake Miners</Box>
                     <Box className="btc-item-right-btn">Stake All</Box>
                   </Box>
@@ -346,7 +430,7 @@ const Mine: React.FC = () => {
                           alignItems="center"
                           justifyContent="space-between"
                         >
-                          <Box display="flex" alignItems="center">
+                          <Box display="flex" flexDirection="column">
                             <Box className="item-title">MBTC Mined2</Box>
                             <Box className="item-price">{item.mined}</Box>
                           </Box>
@@ -363,10 +447,54 @@ const Mine: React.FC = () => {
                     </Box>
                   </Box>
                 ))}
+                {!unStakedList || unStakedList.length == 0 ? <Box>stakedList</Box> : undefined}
               </Grid>
             </Grid>
           </TabPanel>
         </TabContext>
+        <Popover
+          id="simple-popover"
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+        >
+          <Box className="mine-checklist">
+            <List dense className={classes.root}>
+              {minerItem?.map(item => {
+                const labelId = `checkbox-list-secondary-label-${item.id}`;
+                return (
+                  <ListItem key={item.id} button>
+                    <ListItemAvatar>
+                      <Avatar alt={item.name} src={item.image} />
+                    </ListItemAvatar>
+                    <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+                    <ListItemSecondaryAction>
+                      <Checkbox
+                        edge="end"
+                        onChange={handleToggle(item.id)}
+                        // checked={checked.indexOf(item.id) !== -1}
+                        inputProps={{ "aria-labelledby": labelId }}
+                      />
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                );
+              })}
+            </List>
+            <Box className="mine-checklist-btn-box">
+              <Button color="secondary" onClick={handleClose} className="mine-checklist-btn">
+                Choice
+              </Button>
+            </Box>
+          </Box>
+        </Popover>
       </Box>
     </div>
   );
