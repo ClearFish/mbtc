70637e554e9a63301449e2cd894a8c89d46903cb
AbelCodeUp <bxk0129@163.com>
2022-04-17 18:33:57 +0800

    update

--- src/views/Market/Market.tsx
+++ src/views/Market/Market.tsx
@@ -5,12 +5,26 @@ const Market: React.FC = () => {
   return (
     <div id="market-view">
       <div className="banner-view">
+        <div className="market-desc">
+          <div className="title">META BITCOIN NFT</div>
+          <div className="desc">
+            NFT Miner mines MBTC by providing hash power using
+            <br /> proof-of-NFT
+          </div>
+        </div>
         <div className="banner-icon">
           <img src={MarketLogo} alt="" />
         </div>
-        <div className="market-desc">
-          <div className="title">META BITCOIN NFT</div>
-          <div className="desc">NFT Miner mines MBTC by providing hash power using proof-of-NFT</div>
+      </div>
+      <div className="layout-market">
+        <div className="btc-container">
+          <div className="title-container">
+            <div className="select-box">
+              <div className="select-item">Open Market</div>
+              <div className="select-item">New Releases</div>
+            </div>
+            <div className="view-box">view all</div>
+          </div>
         </div>
       </div>
     </div>
