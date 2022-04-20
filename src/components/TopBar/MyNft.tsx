950faabe42f6efab0f4dd06817b8826c34a6436c
qingywen <qingywen@gmail.com>
2022-04-20 12:16:35 +0800

    adjuct margin

--- src/components/TopBar/MyNft.tsx
+++ src/components/TopBar/MyNft.tsx
@@ -1,7 +1,9 @@
 import { Button, Typography, Link } from "@material-ui/core";
+import "./TopBar.scss";
+
 function MyNft() {
   return (
-    <Link href="#/mynft" underline="none">
+    <Link href="#/mynft" underline="none" className="topbar-link-btn">
       <Button id="ohm-menu-button" variant="contained" color="secondary">
         <Typography>My NFT: 4</Typography>
       </Button>
