547c263f06a15af93ab71bb5c9eee95f6eac8459
“timedgqb@qq.com” <timedgqb@qq.com>
2022-04-23 16:53:35 +0800

    [新增代码] 完成POOL页面的开发

--- src/views/Pool/Pool.tsx
+++ src/views/Pool/Pool.tsx
@@ -11,7 +11,19 @@ const Pool: React.FC = () => {
   const { networkId } = useWeb3Context();
   usePathForNetwork({ pathName: "pool", networkID: networkId, history });
 
-  return <div>pool</div>;
+  return (
+    <div id="pool-view">
+      <div className="title">Pool</div>
+      <div className="banner">
+        <div className="btc-logo"></div>
+        <div className="btc-number">100,000,000</div>
+        <div className="btc-amount">≈ $100,000,000</div>
+        <div className="dubai">DUBAI</div>
+        <div className="pool">Pool</div>
+        <div className="h5-bottom">DUBAI Pool</div>
+      </div>
+    </div>
+  );
 };
 
 export default memo(Pool);
