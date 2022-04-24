0396e4138e9c729b76dfc821ba33c016f89220e3
“timedgqb@qq.com” <timedgqb@qq.com>
2022-04-24 15:38:38 +0800

    [新增代码] 完成mine手机端的适配

--- src/views/Mine/Mine.tsx
+++ src/views/Mine/Mine.tsx
@@ -1,5 +1,6 @@
 import "./Mine.scss";
 import MineBanner from "../../assets/images/mine/mine-banner.png";
+import MineBannerH5 from "../../assets/images/mine/mine-banner-h5.png";
 import { memo, useEffect, useState, MouseEvent } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -594,7 +595,8 @@ const Mine: React.FC = () => {
         <Box className="hero-metrics">
           <Grid xs={12} md={12}>
             <Box className="ohm-card-mine">
-              <img className="card-banner" src={MineBanner} />
+              <img className="card-banner pc" src={MineBanner} />
+              <img className="card-banner h5" src={MineBannerH5} />
               <Button className={`batch-btn ${isSmallScreen && "isMobile"}`} onClick={getAllRewards}>
                 Harvest MBTC
               </Button>
